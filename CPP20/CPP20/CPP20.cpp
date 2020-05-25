2c9d568fdeb153de4d7f52227553566f30c210dd
Pieterjan Spoelders <pj.spoelders@gmail.com>
2020-05-25 23:56:46 +0200

    cpp20 test project

---
+++ CPP20/CPP20/CPP20.cpp
@@ -0,0 +1,14 @@
+// CPP20.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+import std.core;
+import std.regex;
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+//new in CPP20:
+//threading
+//lots of <chrono> time related stuff
